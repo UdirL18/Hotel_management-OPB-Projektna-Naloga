833b44c570bca6717355dfc1d609ee69ff947367
KokaljAnja <69453563+KokaljAnja@users.noreply.github.com>
2022-07-14 18:25:23 +0200

    dodajanje tabel

---
+++ PodatkiA/popusti_za_sobo.sql
@@ -0,0 +1,12 @@
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (1, 3, 8, 69.92, 'tip_sobe_id');
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (2, 6, 7, 91.78, 'tip_sobe_id');
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (3, 1, 8, 42.43, 'tip_sobe_id');
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (4, 3, 10, 59.78, 'tip_sobe_id');
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (5, 6, 9, 19.65, 'tip_sobe_id');
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (6, 3, 10, 97.58, 'tip_sobe_id');
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (7, 6, 11, 96.65, 'tip_sobe_id');
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (8, 2, 10, 9.07, 'tip_sobe_id');
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (9, 5, 11, 38.32, 'tip_sobe_id');
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (10, 1, 8, 13.66, 'tip_sobe_id');
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (11, 3, 8, 63.13, 'tip_sobe_id');
+insert into popusti_za_sobo (popust_id, mesec_zacetek, mesec_konec, diskontna_stopnja, tip_sobe_id) values (12, 1, 7, 46.33, 'tip_sobe_id');
