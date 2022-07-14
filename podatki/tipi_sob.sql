d513f0846abf5c575301bf605822eeda70590b99
UdirL18 <lucija.udir@student.fmf.uni-lj.si>
2022-07-14 19:31:41 +0200

    vsi podatki

---
+++ podatki/tipi_sob.sql
@@ -0,0 +1,8 @@
+insert into tipi_sob (tip_sobe_id, tip_sobe_ime, tip_sobe_opis, cena_sobe, zivali, kadilci) values (1, '1 zakonska postelja', 'soba s pogledom na mesto', 191.22, 0, 0);
+insert into tipi_sob (tip_sobe_id, tip_sobe_ime, tip_sobe_opis, cena_sobe, zivali, kadilci) values (2, '2 zakonski postelji', 'soba s pogledom na morje', 175.06, 1, 0);
+insert into tipi_sob (tip_sobe_id, tip_sobe_ime, tip_sobe_opis, cena_sobe, zivali, kadilci) values (3, '1 enojna postelja', 'soba s pogledom na mesto', 131.49, 0, 0);
+insert into tipi_sob (tip_sobe_id, tip_sobe_ime, tip_sobe_opis, cena_sobe, zivali, kadilci) values (4, '2 enojni postelji', 'soba s pogledom na mesto', 116.57, 1, 1);
+insert into tipi_sob (tip_sobe_id, tip_sobe_ime, tip_sobe_opis, cena_sobe, zivali, kadilci) values (5, '1 zakonska postelja', 'soba s pogledom na morje', 188.91, 1, 0);
+insert into tipi_sob (tip_sobe_id, tip_sobe_ime, tip_sobe_opis, cena_sobe, zivali, kadilci) values (6, '2 zakonski postelji', 'soba s pogledom na morje', 128.5, 1, 1);
+insert into tipi_sob (tip_sobe_id, tip_sobe_ime, tip_sobe_opis, cena_sobe, zivali, kadilci) values (7, '1 enojna postelja', 'soba s pogledom na mesto', 165.75, 0, 1);
+insert into tipi_sob (tip_sobe_id, tip_sobe_ime, tip_sobe_opis, cena_sobe, zivali, kadilci) values (8, '2 enojni postelji', 'soba s pogledom na morje', 134.88, 1, 1);
