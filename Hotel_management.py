5b4cdd9ce242992aed22d451249e4def160df456
UdirL18 <lucija.udir@student.fmf.uni-lj.si>
2022-08-03 22:31:31 +0200

    uvoz podatkov

--- Hotel_management.py
+++ Hotel_management.py
@@ -1,10 +1,22 @@
 from bottle import *
 from bottleext import *
-import sqlite3
+#import sqlite3
 import hashlib
 import os
 
-baza_datoteka = 'HotelManagement.db'
+#uvozimo potrebne podatke za povezavo
+import auth_public as auth
+
+# uvozimo psycopg2 neki sprem - nalozi v ukaznem pozivu pip install psycopg2
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+
+#privzete nastavitve
+SERVER_PORT = os.environ.get('BOTTLE_PORT', 8080)
+RELOADER = os.environ.get('BOTTLE_RELOADER', True)
+DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
+
+#conn_datoteka = 'HotelManagement.db'
 debug(True)
 
 def nastaviSporocilo(sporocilo = None):
@@ -21,18 +33,18 @@ static_dir = "./static"
 
 skrivnost = "rODX3ulHw3ZYRdbIVcp1IfJTDn8iQTH6TFaNBgrSkjIulr"
 
-#def preveriUporabnika(): 
-#    username = request.get_cookie("username", secret=skrivnost)
-#    if username:
-#        cur = baza.cursor()    
-#        uporabnik = None
-#        try: 
-#            uporabnik = cur.execute("SELECT * FROM oseba WHERE username = ?", (username, )).fetchone()
-#        except:
-#            uporabnik = None
-#        if uporabnik: 
-#            return uporabnik
-#    redirect('/prijava')
+# #def preveriUporabnika(): 
+# #    username = request.get_cookie("username", secret=skrivnost)
+# #    if username:
+# #        cur = conn.cursor()    
+# #        uporabnik = None
+# #        try: 
+# #            uporabnik = cur.execute("SELECT * FROM oseba WHERE username = ?", (username, )).fetchone()
+# #        except:
+# #            uporabnik = None
+# #        if uporabnik: 
+# #            return uporabnik
+# #    redirect('/prijava')
 
 @route("/static/<filename:path>")
 def static(filename):
@@ -45,7 +57,7 @@ def index():
 ### ZAPOSLENI
 @get('/zaposleni')
 def zaposleni():
-    cur = baza.cursor()
+    cur = conn.cursor()
     zaposleni = cur.execute("SELECT zaposleni_id,ime,priimek,naziv,telefonska_stevilka,email,oddelek_id,naslov_id,hotel_id FROM zaposleni")
     return template('zaposleni.html', zaposleni=zaposleni)
 
@@ -88,7 +100,7 @@ def dodaj_zaposlenega_post():
 #    naslov_id = request.forms.naslov_id
 #    hotel_id = request.forms.hotel_id
 #    oddelek_id = request.forms.oddelek_id
-#    cur = baza.cursor()
+#    cur = conn.cursor()
 #    cur.execute("INSERT INTO zaposleni (zaposleni_id, ime, priimek, naziv, telefonska_stevilka, email,oddelek_id, naslov_id, hotel_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (zaposleni_id, ime, priimek, naziv, telefonska_stevilka, email,oddelek_id, naslov_id, hotel_id))
 #    redirect('/zaposleni')
 
@@ -96,15 +108,15 @@ def dodaj_zaposlenega_post():
 
 @get('/hotelska_veriga')
 def hotelska_veriga():
-    cur = baza.cursor()
-    hotelska_veriga = cur.execute("SELECT ime_hotelske_verige, naslov_glavne_pisarne, email_hotelske_verige, spletna_stran FROM hotelska_veriga")
-    return template('hotelska_veriga.html', hotelska_veriga=hotelska_veriga)
+    # cur = conn.cursor()
+    cur.execute("SELECT ime_hotelske_verige, naslov_glavne_pisarne, email_hotelske_verige, spletna_stran FROM hotelska_veriga")
+    return template('hotelska_veriga.html', hotelska_veriga=cur)
 
 ### HOTEL
 
 @get('/hotel')
 def hotel():
-    cur = baza.cursor()
+    cur = conn.cursor()
     hotel = cur.execute("""SELECT ime_hotela, naslov.mesto, naslov.posta, naslov.drzava, telefonska_stevilka, email, hotelska_veriga.ime_hotelske_verige FROM hotel_podatki
     JOIN naslov ON hotel_podatki.naslov_id=naslov.naslov_id
     JOIN hotelska_veriga ON hotel_podatki.hotelska_veriga_id=hotelska_veriga.hotelska_veriga_id
@@ -115,7 +127,7 @@ def hotel():
 
 @get('/gostje')
 def gostje():
-    cur = baza.cursor()
+    cur = conn.cursor()
     gostje = cur.execute("""SELECT ime,priimek,telefonska_stevilka,email,naslov.mesto, naslov.posta, naslov.drzava FROM gostje
     JOIN naslov ON gostje.naslov_id=naslov.naslov_id
     ORDER BY gostje.priimek""")
@@ -123,122 +135,135 @@ def gostje():
 
 @get('/gostje/dodaj')
 def naslov():
-    cur = baza.cursor()
+    cur = conn.cursor()
     naslovi = cur.execute("SELECT mesto,posta,drzava from naslov")
 
 def dodaj_gosta_get():
     naslovi = cur.execute("SELECT mesto,posta,drzava FROM naslov")
     return template('gost_edit.html', naslovi=naslovi)
 
-@post('/gostje/dodaj') 
-def dodaj_gosta_post():
-    ime = request.forms.ime
-    priimek = request.forms.priimek
-    telefonska_stevilka = request.forms.telefonska_stevilka
-    email = request.forms.email
-    naslov_id = request.forms.naslov_id
-    cur = baza.cursor()
-    cur.execute("INSERT INTO gostje (ime,priimek,telefonska_stevilka,email,naslov_id) VALUES (?, ?, ?, ?, ?)", 
-         (ime,priimek,telefonska_stevilka,email,naslov_id))
-    redirect('/gostje')
+# @post('/gostje/dodaj') 
+# def dodaj_gosta_post():
+#     ime = request.forms.ime
+#     priimek = request.forms.priimek
+#     telefonska_stevilka = request.forms.telefonska_stevilka
+#     email = request.forms.email
+#     naslov_id = request.forms.naslov_id
+#     cur = conn.cursor()
+#     cur.execute("INSERT INTO gostje (ime,priimek,telefonska_stevilka,email,naslov_id) VALUES (?, ?, ?, ?, ?)", 
+#          (ime,priimek,telefonska_stevilka,email,naslov_id))
+#     redirect('/gostje')
 
-### SOBA
+# ### SOBA
 
-@get('/sobe')
-def sobe():
-    cur = baza.cursor()
-    sobe = cur.execute("""SELECT stevilka_sobe,tip_sobe_id,hotel_podatki.ime_hotela FROM sobe
-    JOIN hotel_podatki ON sobe.hotel_id=hotel_podatki.ime_hotela""")
-    return template('sobe.html', sobe=sobe)
-
-
-### REGISTRACIJA, PRIJAVA
-
-def hashGesla(s):
-    m = hashlib.sha256()
-    m.update(s.encode("utf-8"))
-    return m.hexdigest()
-
-@get('/registracija')
-def registracija_get():
-    napaka = nastaviSporocilo()
-    return template('registracija.html', napaka=napaka)
-
-@post('/registracija')
-def registracija_post():
-    zaposleni_id = request.forms.zaposleni_id
-    username = request.forms.username
-    password = request.forms.password
-    password2 = request.forms.password2
-    if zaposleni_id is None or username is None or password is None or password2 is None:
-        nastaviSporocilo('Registracija ni možna') 
-        redirect('/registracija')
-        return
-    cur = baza.cursor()    
-    uporabnik = None
-    try: 
-        uporabnik = cur.execute("SELECT * FROM zaposleni WHERE zaposleni_id = ?", (zaposleni_id, )).fetchone()
-    except:
-        uporabnik = None
-    if uporabnik is None:
-        nastaviSporocilo('Registracija ni možna') 
-        redirect('/registracija')
-        return
-    if len(password) < 4:
-        nastaviSporocilo('Geslo mora imeti vsaj 4 znake.') 
-        redirect('/registracija')
-        return
-    if password != password2:
-        nastaviSporocilo('Gesli se ne ujemata.') 
-        redirect('/registracija')
-        return
-    zgostitev = hashGesla(password)
-    cur.execute("UPDATE zaposleni set username = ?, password = ? WHERE zaposleni_id = ?", (username, zgostitev, zaposleni_id))
-    response.set_cookie('username', username, secret=skrivnost)
-    redirect('/zaposleni')
+# @get('/sobe')
+# def sobe():
+#     cur = conn.cursor()
+#     sobe = cur.execute("""SELECT stevilka_sobe,tip_sobe_id,hotel_podatki.ime_hotela FROM sobe
+#     JOIN hotel_podatki ON sobe.hotel_id=hotel_podatki.ime_hotela""")
+#     return template('sobe.html', sobe=sobe)
 
 
+# ### REGISTRACIJA, PRIJAVA
 
-#@get('/prijava')
-#def prijava_get():
-#    napaka = nastaviSporocilo()
-#    return template('prijava.html', napaka=napaka)
+# def hashGesla(s):
+#     m = hashlib.sha256()
+#     m.update(s.encode("utf-8"))
+#     return m.hexdigest()
 
-#@post('/prijava')
-#def prijava_post():
-#    username = request.forms.username
-#    password = request.forms.password
-#    if username is None or password is None:
-#        nastaviSporocilo('Uporabniško ima in geslo morata biti neprazna') 
-#        redirect('/prijava')
-#        return
-#    cur = baza.cursor()    
-#    hashBaza = None
-#    try: 
-#        hashBaza = cur.execute("SELECT password FROM oseba WHERE username = ?", (username, )).fetchone()
-#        hashBaza = hashBaza[0]
-#    except:
-#        hashBaza = None
-#    if hashBaza is None:
-#        nastaviSporocilo('Uporabniško geslo ali ime nista ustrezni') 
-#        redirect('/prijava')
-#        return
-#    if hashGesla(password) != hashBaza:
-#        nastaviSporocilo('Uporabniško geslo ali ime nista ustrezni') 
-#        redirect('/prijava')
-#        return
-#    response.set_cookie('username', username, secret=skrivnost)
-#    redirect('/komitenti')
+# @get('/registracija')
+# def registracija_get():
+#     napaka = nastaviSporocilo()
+#     return template('registracija.html', napaka=napaka)
+
+# @post('/registracija')
+# def registracija_post():
+#     zaposleni_id = request.forms.zaposleni_id
+#     username = request.forms.username
+#     password = request.forms.password
+#     password2 = request.forms.password2
+#     if zaposleni_id is None or username is None or password is None or password2 is None:
+#         nastaviSporocilo('Registracija ni možna') 
+#         redirect('/registracija')
+#         return
+#     cur = conn.cursor()    
+#     uporabnik = None
+#     try: 
+#         uporabnik = cur.execute("SELECT * FROM zaposleni WHERE zaposleni_id = ?", (zaposleni_id, )).fetchone()
+#     except:
+#         uporabnik = None
+#     if uporabnik is None:
+#         nastaviSporocilo('Registracija ni možna') 
+#         redirect('/registracija')
+#         return
+#     if len(password) < 4:
+#         nastaviSporocilo('Geslo mora imeti vsaj 4 znake.') 
+#         redirect('/registracija')
+#         return
+#     if password != password2:
+#         nastaviSporocilo('Gesli se ne ujemata.') 
+#         redirect('/registracija')
+#         return
+#     zgostitev = hashGesla(password)
+#     cur.execute("UPDATE zaposleni set username = ?, password = ? WHERE zaposleni_id = ?", (username, zgostitev, zaposleni_id))
+#     response.set_cookie('username', username, secret=skrivnost)
+#     redirect('/zaposleni')
+
+
+
+# #@get('/prijava')
+# #def prijava_get():
+# #    napaka = nastaviSporocilo()
+# #    return template('prijava.html', napaka=napaka)
+
+# #@post('/prijava')
+# #def prijava_post():
+# #    username = request.forms.username
+# #    password = request.forms.password
+# #    if username is None or password is None:
+# #        nastaviSporocilo('Uporabniško ima in geslo morata biti neprazna') 
+# #        redirect('/prijava')
+# #        return
+# #    cur = conn.cursor()    
+# #    hashconn = None
+# #    try: 
+# #        hashconn = cur.execute("SELECT password FROM oseba WHERE username = ?", (username, )).fetchone()
+# #        hashconn = hashconn[0]
+# #    except:
+# #        hashconn = None
+# #    if hashconn is None:
+# #        nastaviSporocilo('Uporabniško geslo ali ime nista ustrezni') 
+# #        redirect('/prijava')
+# #        return
+# #    if hashGesla(password) != hashconn:
+# #        nastaviSporocilo('Uporabniško geslo ali ime nista ustrezni') 
+# #        redirect('/prijava')
+# #        return
+# #    response.set_cookie('username', username, secret=skrivnost)
+# #    redirect('/komitenti')
     
-#@get('/odjava')
-#def odjava_get():
-#    response.delete_cookie('username')
-#    redirect('/prijava')
+# #@get('/odjava')
+# #def odjava_get():
+# #    response.delete_cookie('username')
+# #    redirect('/prijava')
 
-### OSTALO
+# ### OSTALO
 
-baza = sqlite3.connect(baza_datoteka, isolation_level=None)
-baza.set_trace_callback(print)
-cur = baza.cursor()
-cur.execute("PRAGMA foreign_keys = ON;")
-run(host='localhost', port=8080, reloader=True)
+# # conn = sqlite3.connect(conn_datoteka, isolation_level=None)
+# # conn.set_trace_callback(print)
+# # cur = conn.cursor()
+# # cur.execute("PRAGMA foreign_keys = ON;")
+# # run(host='localhost', port=8080, reloader=True)
+
+
+# Glavni program
+
+# priklopimo se na bazo
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
+#conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) #za pogovarjanje z bazo
+
+# poženemo strežnik na podanih vratih, npr. http://localhost:8080/
+if __name__ == "__main__":
+    run(host='localhost', port=SERVER_PORT, reloader=RELOADER)
+    print('test')
--- auth_public.py
+++ auth_public.py
@@ -1,4 +1,3 @@
-
 db = 'sem2022_lucijau'
 host = 'baza.fmf.uni-lj.si'
 user = 'javnost'
